ef626b8c45a0d96915c255983c663b771d21f4e3
joaoarruda101 <victorfurquim00@gmail.com>
2023-04-22 23:54:51 -0300

    First commit

---
+++ Dart/Algoritmos/TestaVariavelInteiras.dart
@@ -0,0 +1,11 @@
+void main() {
+  int valor1 = 10;
+  int valor2 = 20;
+  print("eu tenho 25 anos");
+
+  if (valor1 == 10) {
+    print("Idade entrou !!");
+  } else if (valor2 == 30) {
+    print("valor incorreto!");
+  }
+}
